66aefe8454c242e02e8f437eeabe67db870017aa
yanjiemao <mkdirhao@qq.com>
2024-04-26 22:49:48 +0800

    edit base url

--- src/utils/bin/social.ts
+++ src/utils/bin/social.ts
@@ -1,7 +1,13 @@
 import config from "../../../config.json";
 
 export const github = async (args: string[]): Promise<string> => {
-	window.open(`https://github.com/${config.social.github}/`);
+	window.open(`${config.social.github}`);
+
+	return "Opening GitHub...";
+};
+
+export const blog = async (args: string[]): Promise<string> => {
+	window.open(`${config.social.blog}`);
 
 	return "Opening GitHub...";
 };
